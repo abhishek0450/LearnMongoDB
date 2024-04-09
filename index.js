39cb1e0e333eec524641d435ac127d0f7bfafc38
Abhishek Kashyap <108532955+abhishek0450@users.noreply.github.com>
2024-04-09 13:10:03 +0530

    start script in package.json

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const db = require('./db')
 require('dotenv').config();
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;//strategy for authenticating with a username and password
 
 
 
@@ -8,10 +10,40 @@ const app = express()
 const bodyParser = require('body-parser')
 app.use(bodyParser.json());
 
+//middleware
+const logRequest = (req, res, next) => {
+  console.log(`[${new Date().toLocaleString()}] Request Made to : ${req.originalUrl}`);
+  next(); // Move on to the next phase
+}
+app.use(logRequest);
+
+const localAuthMiddleware = passport.authenticate('local', {session: false});
+
 //pizza shop backend
-app.get('/', (req, res) => {
+app.get('/', localAuthMiddleware ,(req, res) => {
   res.send('Welcome to the Pizza Shop!')
 })
+
+
+
+passport.use(new LocalStrategy(async (username, password, done) => {
+    try {
+        // console.log('Received credentials:', username, password);
+        const user = await Person.findOne({ username });
+        if (!user)
+            return done(null, false, { message: 'Incorrect username.' });
+        
+        const isPasswordMatch = await user.comparePassword(password);
+        if (isPasswordMatch)
+            return done(null, user);
+        else
+            return done(null, false, { message: 'Incorrect password.' })
+    } catch (error) {
+        return done(error);
+    }
+}));
+
+app.use(passport.initialize());
 
 
 
@@ -22,7 +54,9 @@ const MenuRoutes = require('./routes/MenuRoutes');
 //use routes
 app.use('/person', personRoutes);
 app.use('/order', MenuRoutes);
-    
+
+
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
--- models/person.js
+++ models/person.js
@@ -34,6 +34,15 @@ const personSchema = new Schema({
     salary: {
         type: Number,
         required: true
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
     }
 
 });
